144bd0a306f79aa521eb81cf6902daf8b689a742
JJ Kavelaars <jjk@uvic.ca>
2020-07-14 16:21:14 -0700

    application to build an SQL db of fake sources.

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     entry_points={
         "console_scripts": [
             "shift_and_stack = dao_mop.sns:main",
-            "train_mop_model = dao_mop.train_model:main"
+            "train_mop_model = dao_mop.train_model:main",
+            "build_plant_db = dao_mop.build_plant_list_db:main",
         ],
     }
 )
