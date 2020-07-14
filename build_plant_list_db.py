c0b24ffba9344bed8e73f69d77bb754add9bbd59
JJ Kavelaars <jjk@uvic.ca>
2020-07-14 16:20:29 -0700

    application to build an SQL db of fake sources.

---
+++ build_plant_list_db.py
@@ -0,0 +1,21 @@
+from dao_mop import data_model
+import logging
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='scan through all the plantList files in a directory and'
+                                                 'create an SQLite3 DB containing those fake sources.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('plant_list_dir', help='Directory containing the .plantList files.')
+    parser.add_argument('--plant-list-db', help='Name of the database to create.', default='plant_list.db')
+    parser.add_argument('--log-level', default='INFO', choices=['DEBUG', 'INFO', 'ERROR'])
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.log_level))
+    plant_list_directory = args.plant_list_dir
+    data_model.build_table_of_planted_sources(plant_list_directory, plant_list_db=args.plant_list_db)
+
+
+if __name__ == '__main__':
+    main()
