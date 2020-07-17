a76bc06bd8678b9a201832ce3ca072f171b7ec8f
JJ Kavelaars <jkavelaars@sns.arbutus>
2020-07-17 00:42:51 +0000

    Make scripts part of the install

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ setup(
     name="daomop",
     version="0.1",
     packages=find_packages(),
+    scripts=['scripts/daomop-stack-cmd'],
     entry_points={
         "console_scripts": [
             "daomop-sns = daomop.sns:main",
-            "daomop-train_cnn = daomop.train_model:main",
-            "daomop-build_plant_db = daomop.build_plant_list_db:main",
+            "daomop-train-cnn = daomop.train_model:main",
+            "daomop-build-plant-db = daomop.build_plant_list_db:main",
         ],
     }
 )
