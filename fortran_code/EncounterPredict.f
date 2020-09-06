720d3e18cd1e7371d05607284070f8514ad9ccb9
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2020-09-06 13:08:03 -0700

    Make a code that will compile

--- fortran_code/EncounterPredict.f
+++ fortran_code/EncounterPredict.f
@@ -1,13 +1,9 @@
 c-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 c 
-c This program compute the position of objects at different times.
-c Should be used once basic detectabilty is determined, ie this uses the 
-c output of NHDetectability.
+c This program computes the observational quantities for a KBO observer
+c from platform described by a trajectory file.
 c
-c This code add 0.7 to the lorri_mag values as Detectability didn't 
-c covert to V from H_r. 
-c
-c Usage: Obj_at_Date model trajectory obs-date
+c Usage: EncounterPredict orbit_file.txt trajectory_file.txt
 c
 c-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
       
@@ -17,15 +13,15 @@ c-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
       implicit none
 
-      integer*4 n_obj_max, screen, keybd, verbose, 
-      integer*4 code, lun_trajectory, line_number
-      integer*4 nw_max, nw, ierr, nchar
+      integer*4 n_obj_max, screen, keybd, verbose
+      integer*4 code, lun_trajectory, lun_orbit, line_number
+      integer*4 nw_max, nw, ierr, nchar, stderr, rierr
+      real*8 tmp, mt
 
       parameter
      $     (n_obj_max = 10000, screen = 6, keybd = 5, verbose = 9,
-     $     nw_max = 30)
-      character line*200, buffer*800,  word(nw_max)*80
-      integer*4 lw(nw_max)
+     $     nw_max = 30, stderr = 0)
+      character buffer*800
 
       real*8 Pi, TwoPi, drad
 
@@ -34,19 +30,18 @@ c-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 
 c     Barycentric Elements
-      real*8 a, e, inc, node, peri, M, epoch, 
+      real*8 a, e, inc, node, peri, M, epoch 
 c     Physical Parameters 
       real*8 h, gb
 C     circumstances
-      real*8 obs_jday, obspos(3), ros, pos
+      real*8 obs_jday, obs_pos(3), ros
 c     Lorri Observer Computed values
-      real*8 ra, dec, r, delta, ph, mag, pos(3)
+      real*8 ra, dec, r, delta, alpha, mag, pos(3)
 
-      parameter (lun_trajectory = 12, code = 500, gb = 0.15)
-
+      parameter (lun_trajectory = 12, lun_orbit = 11, code = 500, 
+     $  gb = 0.15)
 C     inputs
       character orbit_file*200, trajectory_file*200
-     $     word(nw_max)*80
 
       CALL getarg(1, orbit_file)
       orbit_file = trim(orbit_file)
@@ -55,45 +50,44 @@ C     inputs
 
 C     Orbit file should be formated. 
 C     a e inc node peri M epoch
-      open (unit=lun_h, file=orbit_file, status='OLD')
-      read (lun_h, *, err=9999)  a, e, inc, node, peri, M, epoch
-      close (lun_h)
+      open (unit=lun_orbit, file=orbit_file, status='OLD')
+      read (lun_orbit, *, err=9999)  a, e, inc, node, peri, M, epoch
+      close (lun_orbit)
 
       open (unit=lun_trajectory, file=trajectory_file, status='OLD')
 c     Skip First line (should be header line)
       read(lun_trajectory, *) buffer
       if (ierr .ne. 0) then
-         write *, 'Error reading trajectory '//trajectory_file
-         exit(255)
+         write (stderr, *) 'Error reading trajectory '//trajectory_file
+         call exit(255)
       end if
 
 c     First possible date for observation
       obs_jday = -1.0
 
       do while ( rierr < 1 )
-c     Get position of NH at obs_jday
+C        Get position of NH at obs_jday
          call ObsPos(-lun_trajectory, obs_jday, 
-     $        obspos, tmp, ros, ierr)
+     $        obs_pos, tmp, ros, ierr)
 
-c     Get the RA/DEC at date
+C        Get the RA/DEC at date
          mt = M
      $        + (TwoPi/(a**1.5d0*365.25d0))*(obs_jday-epoch)
          mt = mt - int(mt/TwoPi)*TwoPi
          call pos_cart(a, e, inc, node, peri, mt, pos(1),
      $        pos(2), pos(3))
-         call RADECeclXV(pos, obspos, delta, ra, dec)
-c     Compute the magnitude
+         call RADECeclXV(pos, obs_pos, delta, ra, dec)
+C        Compute the magnitude
          r = sqrt(pos(1)**2 + pos(2)**2 + pos(3)**2)
          call AppMag(r, delta, ros, h, gb, alpha, 
      $        mag, ierr) 
+C        Spacecraft observers in open filter
+         mag = mag + 0.7
 
-         lorri_mag = lorri_mag + 0.7
-
-         write (lun_h, '(6(f16.5,1x))', advance='no') obs_jday, 
+         write (screen, '(6(f16.5,1x))', advance='no') obs_jday, 
      $        ra, dec, delta, mag,  alpha
 
  8000 end do
-      close (lun_h)
 
  9999 CONTINUE
       end program EP
