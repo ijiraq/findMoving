47204b6706d9d1f76d4cda3c407e790731629bcd
JJ Kavelaars <jjkavelaars@gmail.com>
2021-02-19 16:53:58 +0000

    Retreive DIFF tarballs as tasks, useful convenience function

--- scripts/daomop-target-sns.sh
+++ scripts/daomop-target-sns.sh
@@ -27,18 +27,31 @@ stack=$1 && shift
 ra=$1 && shift
 dec=$1 && shift
 num=$1 && shift
-if [ ${exptype} == "DIFFEXP" ]
+if [ ${exptype} == "CORR" ]
 then
-    input="diff"
-else
     input="processCcdOutputs"
+else
+    input="diff"
 fi
 vosbase="vos:NewHorizons/S20A-OT04/"
-dbimages="vos:NewHorizons/${index}/dbimages/"
 output="sns" 
 filter="HSC-R2"
 section=750
 chip=$(echo ${ccd} | awk '{printf("%03d",$1)}')
+pointing=$(echo ${pointing} | awk '{printf("%05d",$1)}')
+dbimages="vos:NewHorizons/dbimages/${pointing}/${chip}"
+
+# Get from VOSpace the diff images to be stacked for this sources
+\rm -r ${basedir}/rerun/diff/deepDiff
+vcp  vos:NewHorizons/S20A-OT04/DIFFS/${pointing}/DIFF-${pointing}-${chip}_masked.tar ./
+tar xvf DIFF-${pointing}-${chip}_masked.tar
+rm DIFF-${pointing}-${chip}_masked.tar
+for file in $(find ./HSC_All -name "*_masked.fits" -print)
+do
+    f=`echo ${file} | sed -e 's/_masked//'`
+    mv ${file} ${f}
+done
+
 
 echo "Stacking ${section}X${section}  pixel box around ${ra} ${dec} at rate: ${rate} angle: ${angle}"
 daomop-sns  ${basedir}\
@@ -69,5 +82,6 @@ do
     ccd=$(echo ${ccd%.fits} | awk -Fp '{printf("ccd%02d", $1)}')
     vmkdir -p ${dbimages}/${expnum}/${ccd} || exit
     mcp ${stack} ${dbimages}/${expnum}/${ccd}/ || exit
+    rm ${stack}
 done
 
