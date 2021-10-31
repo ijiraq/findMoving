f6b74be1a29bdd7a124da4e97f324a016b2fe3fc
JJ Kavelaars <jjkavelaars@gmail.com>
2021-10-30 21:31:00 -0700

    Mark previously measured sources with cyan

--- src/daomop/measure_kbo.py
+++ src/daomop/measure_kbo.py
@@ -110,6 +110,7 @@ def load_images(images, ra, dec, wcs_dict, orbit=None, dra=None, ddec=None,
                     if record_index == obsdate.mpc[0:14]:
                         colour = 'cyan' and not obs.null_observation or 'red'
                         circle = f'icrs; circle({obs.coordinate.ra.degree}d,{obs.coordinate.dec.degree}d,5p) # color={colour}'
+                        print(circle)
                         ds9.set('regions', circle)
                         break
             else:
