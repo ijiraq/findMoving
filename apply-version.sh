4eb4012acf157ef46e364371d099665a97b11a1e
JJ Kavelaars <JJ.Kavelaars@nrc-cnrc.gc.ca>
2024-03-14 16:15:06 -0700

    cleanup un-needed config files

--- apply-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. VERSION && echo "tags: $TAGS"
-for t in $TAGS; do
-   docker image tag images.canfar.net/uvickbos/find_moving:latest images.canfar.net/uvickbos/find_moving:$t
-done
-unset TAGS
-docker image list images.canfar.net/uvickbos/find_moving
